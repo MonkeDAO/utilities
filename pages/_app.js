b193843dacc458a3f3331692cccd7785f22e016e
rob shaw <rob@statik.dev>
2022-07-21 00:32:19 -0400

    refactor: move wallet/metaplex contexts into app for use across site

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,56 @@
+import { useMemo, useState } from 'react';
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
 import { Toaster } from 'react-hot-toast';
 
+import { MetaplexProvider } from './MetaplexProvider';
+
+// globals MUST come before wallet adapter styling
 import '../styles/globals.css';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 function MyApp({ Component, pageProps }) {
+  const rpcHost = 'https://monketfza2mzfxcgg2gdda9dltmjn.xyz2.hyperplane.dev/';
+  const [network, setNetwork] = useState(WalletAdapterNetwork.Mainnet);
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
   return (
-    <>
-      <Toaster position="bottom-center" />
-      <Component {...pageProps} />
-    </>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <MetaplexProvider>
+            <Toaster position="bottom-center" />
+            <Component {...pageProps} />
+          </MetaplexProvider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,16 @@
-import styles from '../styles/Home.module.css';
-import { useMemo, useState, useEffect } from 'react';
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-  GlowWalletAdapter,
-  PhantomWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-import { MetaplexProvider } from './MetaplexProvider';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
 import { HolderArea } from './HolderArea';
+
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 export default function Home() {
-  const rpcHost = 'https://monketfza2mzfxcgg2gdda9dltmjn.xyz2.hyperplane.dev/';
-  const [network, setNetwork] = useState(WalletAdapterNetwork.Mainnet);
-
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new GlowWalletAdapter(),
-      new SlopeWalletAdapter(),
-      new SolflareWalletAdapter({ network }),
-      new TorusWalletAdapter(),
-    ],
-    [network]
-  );
-
   return (
     <div className="flex min-w-min min-h-screen flex-col items-center justify-center py-10 bg-monke-cream">
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>
-            <MetaplexProvider>
-              <div className="flex flex-col items-center justify-center">
-                <WalletMultiButton />
-                <HolderArea />
-              </div>
-            </MetaplexProvider>
-          </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
+      <div className="flex flex-col items-center justify-center">
+        <WalletMultiButton />
+        <HolderArea />
+      </div>
     </div>
   );
 }
