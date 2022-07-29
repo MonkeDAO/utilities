263ddcbb779aaed7b730f1e7f613c2649c69d03e
rob shaw <rob@statik.dev>
2022-07-29 18:05:49 -0400

    fix: handle localstorage check when next.js attempts ssr

--- hooks/useToken.ts
+++ hooks/useToken.ts
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
 
-
-import { useState } from 'react';
+type Token = { token?: string; hw?: string; txn?: string };
 
 export default function useToken() {
   const getToken = () => {
-    const tokenString = typeof window === "undefined" ? '{}' : sessionStorage.getItem('token');
-    const userToken = tokenString && JSON.parse(tokenString);
-    return { token: userToken?.token, hw: userToken?.hw, txn: userToken?.txn };
+    try {
+      const tokenString =
+        typeof window === 'undefined' ? '{}' : sessionStorage.getItem('token');
+      const userToken = tokenString && JSON.parse(tokenString);
+      return {
+        token: userToken?.token,
+        hw: userToken?.hw,
+        txn: userToken?.txn,
+      };
+    } catch (e) {
+      return { token: undefined, hw: undefined, txn: undefined };
+    }
   };
 
-  const [token, setToken] = useState(getToken());
+  const [token, setToken] = useState<Token>();
+  useEffect(() => {
+    setToken(getToken());
+  }, []);
 
   const saveToken = (userToken: any) => {
     sessionStorage.setItem('token', JSON.stringify(userToken));
